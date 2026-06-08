8b436b3170311384093bcf0f87e62e131c6fee08
Dev Ga Mo <vinhlephu.vinhlephu@gmail.com>
2026-06-08 11:32:36 +0700

    feat: configure production native ad unit id

--- src/config/admob.ts
+++ src/config/admob.ts
@@ -10,7 +10,7 @@ export const ADMOB_REWARDED_CAPSULE_SLOT_AD_UNIT_ID =
   Platform.OS === 'android' ? ADMOB_REWARDED_CAPSULE_SLOT_PRODUCTION_AD_UNIT_ID : '';
 
 export const ADMOB_NATIVE_PRODUCTION_AD_UNIT_ID =
-  'ca-app-pub-5234300032655235/1234567890'; // Nhà phát triển sẽ cấu hình ID thực tế ở đây
+  'ca-app-pub-5234300032655235/6672944465'; // Đã cập nhật ID thực tế của bạn
 
 export const ADMOB_NATIVE_AD_UNIT_ID =
   Platform.OS === 'android'
